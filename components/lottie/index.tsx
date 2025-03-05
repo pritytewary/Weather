5e9fbd5f8c6f52a56ffde7fe0f34f1c3e8437279
Prity Tewary <pritytewary6@gmail.com>
2025-03-05 22:56:32 +0530

    Dynamically import Lottie

--- components/lottie/index.tsx
+++ components/lottie/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import Lottie from "lottie-react";
 import { useEffect, useRef, useState } from "react";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 export default function LottieIcon({
   name,
